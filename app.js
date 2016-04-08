0d630329abae632cd199f022be7347ad2c172f9d
aaroncampbell2311 <aaroncampbell2311@gmail.com>
2016-04-07 20:08:28 -0600

    added expenses routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var expenses = require('./routes/expenses');
 
 var app = express();
 
@@ -34,6 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/', expenses);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
